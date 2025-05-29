4a1a2713de83035952bed72dbc6ed6390e2bde04
miih <santos.lima.emilly@escola.pr.gov.br>
2025-05-29 12:12:58 -0300

    exercicio 1

---
+++ exercicio 1.py
@@ -0,0 +1,12 @@
+num1 = int(input("digite o primeiro numero "))
+num2 = int(input("digite o segundo numero"))
+
+a = num1 + num2
+b = num1 - num2
+c = num1 * num2
+d = num1 / num2
+
+print("a soma é igual a:", a )
+print("a diferença é igual a:", b )
+print( "a multiṕlicação é igual a:", c )
+print( "a divisão é igual a:", d )
